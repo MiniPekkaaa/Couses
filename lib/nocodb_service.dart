da0d2e3093b8bf3ab55fbe60c2228bca8c2d73c6
Roman Otto <afrin2021@mail.ru>
2025-05-05 16:15:02 +0700

    05.05.25 3

--- lib/nocodb_service.dart
+++ lib/nocodb_service.dart
@@ -1,37 +1,37 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class NocoDBService {
-  static const String apiUrl = 'http://46.101.121.75:8080/api/v2/tables';
-  static const String apiKey = 'g68IOq9S9s5zyC2KSv4Ld4z5lNVvbpKLVd7SyClP';
-
-  // ID таблиц из вашего API
-  static const String coursesTableId = 'm2dpm6io3n343mn';
-  static const String lessonsTableId = 'm2inpyz3ugqrvgz';
+class AirtableService {
+  static const String apiKey = 'patjbZurod2EX97nU.c63a5194242fb65ddc7eaa406a1be34e020596b9defb130953f75b3b3daf2e62';
+  static const String baseId = 'appepjeg5qmyfe7sf';
+  static const String coursesTable = 'tblkJW1h5dLTW6dwJ';
+  static const String lessonsTable = 'tblE4Nb1q3vvzT1eX'; // Укажите id таблицы Lessons, если есть
 
   static Future<List<Map<String, dynamic>>> fetchCourses() async {
+    final url = 'https://api.airtable.com/v0/$baseId/$coursesTable';
     final response = await http.get(
-      Uri.parse('$apiUrl/$coursesTableId/records'),
-      headers: {'xc-token': apiKey},
+      Uri.parse(url),
+      headers: {'Authorization': 'Bearer $apiKey'},
     );
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      return List<Map<String, dynamic>>.from(data['list']);
+      return List<Map<String, dynamic>>.from(data['records'].map((r) => r['fields']));
     } else {
-      throw Exception('Ошибка загрузки курсов');
+      throw Exception('Ошибка загрузки курсов: ${response.body}');
     }
   }
 
-  static Future<List<Map<String, dynamic>>> fetchLessons(dynamic courseId) async {
+  static Future<List<Map<String, dynamic>>> fetchLessons(String courseRecordId) async {
+    final url = 'https://api.airtable.com/v0/$baseId/$lessonsTable?filterByFormula=FIND(\'' + courseRecordId + '\', {fldGy90lQ9sfZQsEA})';
     final response = await http.get(
-      Uri.parse('$apiUrl/$lessonsTableId/records?where=course_id,eq,$courseId'),
-      headers: {'xc-token': apiKey},
+      Uri.parse(url),
+      headers: {'Authorization': 'Bearer $apiKey'},
     );
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      return List<Map<String, dynamic>>.from(data['list']);
+      return List<Map<String, dynamic>>.from(data['records'].map((r) => r['fields']));
     } else {
-      throw Exception('Ошибка загрузки уроков');
+      throw Exception('Ошибка загрузки уроков: ${response.body}');
     }
   }
 } 
