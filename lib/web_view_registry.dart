91130501cd36391264d20ec2d3a3b9ff49e54e78
Roman Otto <afrin2021@mail.ru>
2025-05-06 17:00:19 +0700

    видео 17

--- lib/web_view_registry.dart
+++ lib/web_view_registry.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart' show registerViewFactory;
 import 'package:universal_html/html.dart' as html;
 
 void registerWebViewFactory(String viewType, dynamic Function(int) factory) {
