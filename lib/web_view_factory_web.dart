9767fe7581da98081756641779db7126f61d6797
Roman Otto <afrin2021@mail.ru>
2025-05-06 17:12:21 +0700

    ВИДЕО 25

--- lib/web_view_factory_web.dart
+++ lib/web_view_factory_web.dart
@@ -1,10 +1,11 @@
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:ui' as ui;
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:universal_html/html.dart' as html;
 
 void registerKinescopeViewFactory(String viewType, String url) {
   // ignore: undefined_prefixed_name
-  ui.platformViewRegistry.registerViewFactory(viewType, (int viewId) {
+  registerViewFactory(viewType, (int viewId) {
     final iframe = html.IFrameElement()
       ..src = url
       ..style.border = 'none'
