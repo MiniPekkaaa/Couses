493e62cfa1cd6111368dc0fabd3c8a5426d3e2e5
Roman Otto <afrin2021@mail.ru>
2025-05-06 17:07:35 +0700

    видео 22

--- lib/web_view_factory.dart
+++ lib/web_view_factory.dart
@@ -1,19 +1,15 @@
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:universal_html/html.dart' as html;
 import 'dart:ui' as ui;
 
 void registerKinescopeViewFactory(String viewType, String url) {
-  // Регистрируем view factory для web
-  if (kIsWeb) {
-    // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(viewType, (int viewId) {
-      final iframe = html.IFrameElement()
-        ..src = url
-        ..style.border = 'none'
-        ..allowFullscreen = true
-        ..width = '100%'
-        ..height = '315';
-      return iframe;
-    });
-  }
+  // ignore: undefined_prefixed_name
+  ui.platformViewRegistry.registerViewFactory(viewType, (int viewId) {
+    final iframe = html.IFrameElement()
+      ..src = url
+      ..style.border = 'none'
+      ..allowFullscreen = true
+      ..width = '100%'
+      ..height = '315';
+    return iframe;
+  });
 } 
